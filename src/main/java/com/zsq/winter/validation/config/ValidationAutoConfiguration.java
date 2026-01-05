ba1d035f8a054b4114f9462422393779472178cb
zhushengqian <3309997659@qq.com>
2026-01-05 14:43:04 +0800

    chore(deps): 精简和调整依赖配置
    - 替换spring-boot-starter-web和spring-boot-starter-aop为spring-context
    - 移除spring-boot-configuration-processor和spring-boot-autoconfigure依赖
    - 删除spring-configuration-metadata.json文件及相关Winter校验配置
    - 清理ValidationAutoConfiguration类，移除不必要的注解和引入内容

--- src/main/java/com/zsq/winter/validation/config/ValidationAutoConfiguration.java
+++ src/main/java/com/zsq/winter/validation/config/ValidationAutoConfiguration.java
@@ -1,11 +1,5 @@
 package com.zsq.winter.validation.config;
 
-import com.zsq.winter.validation.provider.DictDataProvider;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
